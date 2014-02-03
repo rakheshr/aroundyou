8b99ab8be7c86e5685d4b8c8a77eac810179b7f3
anshah <ankitshah_17@yahoo.com>
2014-02-02 18:53:40 -0800

    Adding oauth 2.0

---
+++ AroundYou/AroundYou/YelpClient.h
@@ -0,0 +1,17 @@
+//
+//  YelpClient.h
+//  AroundYou
+//
+//  Created by Ankit Nitin Shah on 2/2/14.
+//  Copyright (c) 2014 Yahoo. All rights reserved.
+//
+
+#import "AFOAuth2Client.h"
+
+@interface YelpClient : AFOAuth2Client
+
++(YelpClient *) instance;
+
+
+
+@end
---
+++ AroundYou/AroundYou/YelpClient.m
@@ -0,0 +1,13 @@
+//
+//  YelpClient.m
+//  AroundYou
+//
+//  Created by Ankit Nitin Shah on 2/2/14.
+//  Copyright (c) 2014 Yahoo. All rights reserved.
+//
+
+#import "YelpClient.h"
+
+@implementation YelpClient
+
+@end
